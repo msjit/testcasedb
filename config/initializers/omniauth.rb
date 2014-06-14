86a6e7d29418e87a88132579b96105a0f90a17b4
Marc Joffe <mjoffe@msjit.ca>
2014-06-14 17:04:51 -0400

    Minor tweak to omniauth settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Setting.find_by_name('Google Auth enabled').value == '1'
+  if Setting.find_by_name('Google Auth enabled') && Setting.find_by_name('Google Auth enabled').value
     provider :google_oauth2, Setting.find_by_name('Google Auth Client ID').value, Setting.find_by_name('Google Auth Secret').value
   end
 end
--- db/migrate/20140605100724_create_settings_google_auth.rb
+++ db/migrate/20140605100724_create_settings_google_auth.rb
@@ -1,8 +1,8 @@
 class CreateSettingsGoogleAuth < ActiveRecord::Migration
   def up
-    Setting.find_or_create_by_name(:name => 'Google Auth enabled', :value => true, :description => 'Enable Google Auth.')
-    Setting.find_or_create_by_name(:name => 'Google Auth Client ID', :value => '365073516956-44957ki7m1v145l9n6gs2sfrc34f05u4.apps.googleusercontent.com', :description => 'Google Auth Client ID.')
-    Setting.find_or_create_by_name(:name => 'Google Auth Secret', :value => 'N2bHxPfTjMprcaxhW-jHr4MN', :description => 'Google Auth Secret')
+    Setting.find_or_create_by_name(:name => 'Google Auth enabled', :value => 'Enabled', :description => 'Enable Google Auth.')
+    Setting.find_or_create_by_name(:name => 'Google Auth Client ID', :value => '')
+    Setting.find_or_create_by_name(:name => 'Google Auth Secret', :value => '', :description => 'Google Auth Secret')
   end
 
   def down
