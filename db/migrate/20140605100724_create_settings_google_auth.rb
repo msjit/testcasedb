96c776a5e23dc587330ee724ca63876d7abf11f9
Marc Joffe <mjoffe@msjit.ca>
2014-06-26 10:32:55 -0400

    Google auth should be disabled by default

--- db/migrate/20140605100724_create_settings_google_auth.rb
+++ db/migrate/20140605100724_create_settings_google_auth.rb
@@ -1,6 +1,6 @@
 class CreateSettingsGoogleAuth < ActiveRecord::Migration
   def up
-    Setting.find_or_create_by_name(:name => 'Google Auth enabled', :value => 'Enabled', :description => 'Enable Google Auth.')
+    Setting.find_or_create_by_name(:name => 'Google Auth enabled', :value => 'Disabled', :description => 'Enable Google Auth.')
     Setting.find_or_create_by_name(:name => 'Google Auth Client ID', :value => '')
     Setting.find_or_create_by_name(:name => 'Google Auth Secret', :value => '', :description => 'Google Auth Secret')
   end
