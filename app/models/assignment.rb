cb7b844aff240b8629353589249da082b85a1144
Marc Joffe <mjoffe@msjit.ca>
2014-09-22 23:53:22 -0400

    [37] Fix relationship between tasks and assignments. Delete task when deleting an assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -6,7 +6,7 @@ class Assignment < ActiveRecord::Base
 	belongs_to :stencil
 	has_many :results, :dependent => :destroy
   has_many :test_cases, :through => :results
-  has_one :task
+  has_one :task, :dependent => :destroy
 
 	accepts_nested_attributes_for :task
   attr_accessible :notes, :product_id, :version_id, :test_plan_id, :stencil_id, :schedule_id, :task_attributes
