ef4944a318419f365f9d2cdabd5c0b36df425ff7
Marc Joffe <mjoffe@msjit.ca>
2014-09-30 01:05:41 -0400

    [42] Allow blank passwords for users. Users without passwords cannot login, but can use google OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
-    c.merge_validates_length_of_password_field_options :minimum => 8
+    # we disabled the validations. HAve to manually validate in new validation function
+    # This is to allow blank passwords for users
+    # c.merge_validates_length_of_password_field_options :minimum => 8
+    c.validate_password_field = false
     c.logged_in_timeout = TestDB::Application.config.session_timeout.minutes 
   end
   
@@ -10,6 +13,7 @@ class User < ActiveRecord::Base
   validates :username, :uniqueness => true
   validates :email, :presence => true
   validates :role, :presence => true
+  validates_confirmation_of :password, unless: lambda {|user| user.password.blank? }
   has_many :authentications, :dependent => :destroy
   has_many :reports
   has_many :tasks
@@ -17,6 +21,7 @@ class User < ActiveRecord::Base
   has_many :schedules, :through => :schedule_users
   has_many :product_users, :dependent => :destroy
   has_many :products, :through => :product_users
+  validate :validate_password, unless: lambda {|user| user.password.blank? }
   
   def name_with_email
       "#{last_name}, #{first_name} - #{email}"
@@ -25,5 +30,14 @@ class User < ActiveRecord::Base
   # Returns an authorization for a provider type
   def auth_for(auth_provider)
     self.authentications.where(:provider => auth_provider).first
+  end
+  
+  def validate_password
+    if password.length < 8
+      errors.add(:password, 'is too short. Must be at least 8 characters.') 
+    end
+    if password != password_confirmation
+      errors.add(:password, "confirmation does not match password")
+    end
   end
 end
